3f8dca4bbe903f2674745f2e8a1f24686ccbbc51
kwinnik <kwinnik@.(none)>
2014-01-30 14:52:34 -0500

    refactored StackAction

--- Assets/MenuStackManager/Source/StackAction.cs
+++ Assets/MenuStackManager/Source/StackAction.cs
@@ -4,11 +4,54 @@ namespace MenuStackManager
 {
 	public class StackAction : MonoBehaviour {
 		
+		public enum TypeEnum
+		{
+			Intro = 0,
+			Outro,
+			Push,
+			Pop
+		}
+		public bool Intro 
+		{
+			get
+			{
+				return (Type == TypeEnum.Intro || Type == TypeEnum.Pop);
+			}
+		}
+		public TypeEnum Type;
+		public bool Blocking = true;
 		virtual public IEnumerator Action(IEnumerator parent)
 		{
-			if(parent != null)
-				yield return StartCoroutine(parent);
+
+			Coroutine parentCoroutine = null;
+
+			if(Intro && parent != null)
+			{
+				parentCoroutine = StartCoroutine(parent);
+				if(Blocking)
+					yield return parentCoroutine;
+			}
+			
+			if(!Intro && !Blocking && parent != null)
+			{
+				parentCoroutine = StartCoroutine(parent);
+			}
+
+			yield return StartCoroutine(Move());
+
+			if(!Intro && Blocking && parent != null)
+			{
+				parentCoroutine = StartCoroutine(parent);
+			}
+			
+			if(parentCoroutine != null)
+				yield return parentCoroutine;
 			yield return 0;
+		}
+
+		virtual protected IEnumerator Move()
+		{
+			yield break;
 		}
 		
 		public Coroutine ActionCoroutine(IEnumerator parent)
--- Assets/MenuStackManager/Source/StackActionAnimation.cs
+++ Assets/MenuStackManager/Source/StackActionAnimation.cs
@@ -3,46 +3,22 @@ using System.Collections;
 namespace MenuStackManager
 {
 	public class StackActionAnimation : StackAction {
-		public enum TypeEnum
-		{
-			Intro = 0,
-			Outro,
-			Push,
-			Pop
-		}
 		public Animator CurrentAnimator;
-		public bool Intro 
-		{
-			get
-			{
-				return (Type == TypeEnum.Intro || Type == TypeEnum.Pop);
-			}
-		}
-		public TypeEnum Type;
-		public bool Blocking = true;
 		override public IEnumerator Action(IEnumerator parent)
 		{
 			CurrentAnimator.SetTrigger(Type.ToString());
 
 			CurrentAnimator.Update(0);
 			CurrentAnimator.speed = 0;
-			Coroutine parentCoroutine = null;
-			if(Intro)
-			{
-				parentCoroutine = StartCoroutine(base.Action(parent));
-				if(Blocking)
-					yield return parentCoroutine;
-			}
-			
-			if(!Intro && !Blocking)
-			{
-				parentCoroutine = StartCoroutine(base.Action(parent));
-			}
 
+			yield return StartCoroutine(base.Action (parent));
+		}		
+
+		override protected IEnumerator Move()
+		{
 			CurrentAnimator.speed = 1;
 			CurrentAnimator.Update(0);
 			yield return new WaitForFixedUpdate();
-
 
 			float start = CurrentAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime;
 			float diff = (CurrentAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime - start);
@@ -52,15 +28,7 @@ namespace MenuStackManager
 				yield return new WaitForFixedUpdate();
 			}
 
-			if(!Intro && Blocking)
-			{
-				parentCoroutine = StartCoroutine(base.Action(parent));
-			}
-
-			if(parentCoroutine != null)
-				yield return parentCoroutine;
-			yield return 0;
-		}		
-
+			yield break;
+		}
 	}
 }
