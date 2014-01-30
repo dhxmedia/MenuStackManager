24d87dc06053a5113beacbdbd82da5be270e7f6b
kwinnik <kwinnik@.(none)>
2014-01-30 11:19:34 -0500

    added non blocking example

--- Assets/MenuStackManager/Sample/Scripts/MenuManagerFirstLoad.cs
+++ Assets/MenuStackManager/Sample/Scripts/MenuManagerFirstLoad.cs
@@ -4,8 +4,6 @@ using MenuStackManager;
 public class MenuManagerFirstLoad : MonoBehaviour {
 	
 	public GameObject testPrefab;
-	public MenuStackManager.StackAction IntroAction;
-	public MenuStackManager.StackAction OutroAction;
 	private Manager _manager;
 
 	// Use this for initialization
@@ -13,7 +11,7 @@ public class MenuManagerFirstLoad : MonoBehaviour {
 	void Start () {
 		Application.targetFrameRate = 60;
 		_manager = GetComponent<MenuStackManager.Manager>();
-		_manager.PushMenu(testPrefab, OutroAction, IntroAction, null);
+		_manager.PushMenu(testPrefab, null, null, null);
 	}
 	
 	// Update is called once per frame
